83ca884a22c4dbc4d465778afa267580b1337e99
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-07-15 11:44:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,9 +84,9 @@ prediction_proba = clf.predict_proba(input_row)
 
 df_prediction_proba = pd.DataFrame(prediction_proba)
 df_prediction_proba.columns = ['Adelie', 'Chinstrap', 'Gentoo']
-df_prediction_proba
-
-
+prediction_proba.rename(columns={0: 'Adelie',
+                                 1: 'Chinstrap',
+                                 2: 'Gentoo'})
 
 
 
