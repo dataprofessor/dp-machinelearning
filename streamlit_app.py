aead54b84947a654ee36ca815897521805529ac5
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-07-15 11:48:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,4 +87,9 @@ df_prediction_proba.columns = ['Adelie', 'Chinstrap', 'Gentoo']
 df_prediction_proba.rename(columns={0: 'Adelie',
                                  1: 'Chinstrap',
                                  2: 'Gentoo'})
-df_prediction_proba
+# df_prediction_proba
+
+# Display predicted species
+st.subheader('Predicted Species')
+penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
+st.success(str(penguins_species[prediction][0]))
