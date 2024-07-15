a628eacac2b38331f00a16800b5240df4b19689b
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-07-15 11:30:45 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,6 +53,9 @@ target_mapper = {'Adelie': 0,
 def target_encode(val):
   return target_mapper[val]
 
+y = y_raw.apply(target_encode)
+y
+
 with st.expander('Input features'):
   st.write('**Input penguin**')
   input_df
