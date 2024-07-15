48661ee7014d5ab64f208ade7ba4d31b71335930
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-07-15 10:56:18 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ st.title('🤖 Machine Learning App')
 
 st.info('This is app builds a machine learning model!')
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-df
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
