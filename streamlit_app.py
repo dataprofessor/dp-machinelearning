42766b45d4ef79561826d29ba0ddf4639246781c
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-07-15 11:12:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,5 +39,8 @@ with st.sidebar:
           'body_mass_g', body_mass_g,
           'gender', gender}
   input_df = pd.DataFrame(data, index=[0])
+  input_penguins = pd.concat([input_df, X], axis=0)
 
-input_df
+input_penguins
+
+
