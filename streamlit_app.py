3607b5a1a75723dd046b0fe6c77e6aff90d491dc
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-07-15 11:40:26 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,7 +82,8 @@ clf.fit(X, y)
 prediction = clf.predict(input_row)
 prediction_proba = clf.predict_proba(input_row)
 
-prediction_proba
+prediction_proba.columns = ['Adelie', 'Chinstrap', 'Gentoo']
+
 
 
 
